40d43e317ffb1448721975a7c03f4619d8d9fc23
Amin Arria <amin.arria@lambdaclass.com>
2018-11-21 17:38:12 -0300

    Use apt and flags

--- install_script.sh
+++ install_script.sh
@@ -1,7 +1,7 @@
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
 sudo dpkg -i erlang-solutions_1.0_all.deb
-yes | sudo apt-get update
-yes | sudo apt-get install esl-erlang build-essential git gnuplot libtemplate-perl byobu tsung
+apt update
+apt install -y esl-erlang build-essential git gnuplot libtemplate-perl byobu tsung
 git clone git@github.com:lambdaclass/million_requests_per_second.git
 
 # https://github.com/processone/tsung/issues/276
