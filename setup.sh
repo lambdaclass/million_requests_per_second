bb5aa68e75be0c95ece7517127cbcd2b531eb211
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2018-11-21 15:01:48 -0300

    Now setup.sh correctly sets limit of file descriptors to 2M

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sysctl -w fs.file-max=12000500
 sysctl -w fs.nr_open=20000500
-ulimit -n 20000000
+prlimit --pid=$PPID --nofile=2000000
 sysctl -w net.ipv4.tcp_mem='10000000 10000000 10000000'
 sysctl -w net.ipv4.tcp_rmem='1024 4096 16384'
 sysctl -w net.ipv4.tcp_wmem='1024 4096 16384'
