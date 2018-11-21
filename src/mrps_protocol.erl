270157a6a8438144fab9c43d2b241044bd2f8a1f
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2018-11-21 18:35:51 -0300

    Protocol loop implemented without gen_server

--- src/mrps_protocol.erl
+++ src/mrps_protocol.erl
@@ -1,67 +1,50 @@
 -module(mrps_protocol).
 
 -behaviour(ranch_protocol).
--behaviour(gen_server).
 
--export([start_link/4]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
+-export([start_link/4, init/4]).
 
-
-%% ranch_protocol
 start_link(ListenerPid, Socket, Transport, [Register]) ->
-    Pid = proc_lib:spawn_link(?MODULE, init, [[ListenerPid, Socket, Transport, Register]]),
+    Pid = spawn_link(?MODULE, init, [ListenerPid, Socket, Transport, Register]),
     {ok, Pid}.
 
-%% gen_server
-init([ListenerPid, _Socket, Transport, Register]) ->
+init(ListenerPid, _Socket, Transport, Register) ->
     {ok, Socket} = ranch:handshake(ListenerPid),
     Register:store_client(self()),
     ok = Transport:setopts(Socket, [{nodelay, true}, {active, once}]),
 
     Transport:send(Socket, <<"connected\n">>),
-    gen_server:enter_loop(?MODULE, [], 
-        #{socket => Socket, transport => Transport, register => Register}).
+    loop(Socket, Transport, Register).
 
-handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
-
-handle_cast({msg, Message}, State=#{socket := Socket, transport := Transport}) ->
-    Transport:send(Socket, Message),
-    {noreply, State};
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-handle_info({tcp, _Socket, <<"SEND", Message/binary>>}, 
-            State=#{socket := Socket, transport := Transport, register := Register}) ->
-    Register:for_each(send_msg(Message, self())),
-    ok = Transport:setopts(Socket, [{active, once}]),
-    {noreply, State};
-handle_info({tcp, _Socket, <<"COUNT\n">>}, 
-            State=#{socket := Socket, transport := Transport, register := Register}) ->
-    Count = Register:count(),
-    BinaryCount = list_to_binary(integer_to_list(Count)),
-    ok = Transport:send(Socket, [BinaryCount, <<"\n">>]),
-    ok = Transport:setopts(Socket, [{active, once}]),
-    {noreply, State};
-handle_info({tcp, _Socket, _Data}, 
-            State=#{socket := Socket, transport := Transport}) ->
-    ok = Transport:setopts(Socket, [{active, once}]),
-    {noreply, State};
-handle_info({tcp_closed, _Socket}, State) ->
-    {stop, normal, State};
-handle_info({tcp_error, _Socket, Reason}, State) ->
-    {stop, Reason, State};
-handle_info(_Info, State) ->
-    {noreply, State}.
-
-terminate(_Reason, #{socket := Socket, transport := Transport, register := Register}) ->
-    ok = Transport:close(Socket),
-    Register:remove_client(self()),
-    ok.
+loop(Socket, Transport, Register) ->
+    receive
+        {msg, Message} ->
+            Transport:send(Socket, Message),
+			loop(Socket, Transport, Register);
+		{tcp, Socket, <<"SEND", Message/binary>>} ->
+            Register:for_each(send_msg(Message, self())),
+			ok = Transport:setopts(Socket, [{active, once}]),
+			loop(Socket, Transport, Register);
+		{tcp, Socket, <<"COUNT\n">>} ->
+            Count = Register:count(),
+            BinaryCount = list_to_binary(integer_to_list(Count)),
+            Transport:send(Socket, [BinaryCount, <<"\n">>]),
+            ok = Transport:setopts(Socket, [{active, once}]),
+			loop(Socket, Transport, Register);            
+		{tcp_closed, Socket} ->
+            close(Socket, Transport, Register);
+        {tcp_closed, Socket, _Reason} ->
+            close(Socket, Transport, Register)
+	end.
 
 send_msg(Message, Sender) ->
     fun (Client) when Client =:= Sender ->
             pass;
         (Client) ->
-            gen_server:cast(Client, {msg, Message})
+            Client ! {msg, Message}
     end.
+
+close(Socket, Transport, Register) ->
+    ok = Transport:close(Socket),
+    Register:remove_client(self()),
+    stop.
