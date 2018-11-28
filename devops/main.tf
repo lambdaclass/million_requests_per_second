dbac8f9c6634e33149be6249ccf5e457d8682a14
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2018-11-28 12:20:03 -0300

    Provisioning server and clients with Ansible

---
+++ devops/main.tf
@@ -0,0 +1,47 @@
+provider "scaleway" {
+  organization = "${var.organization}"
+  token        = "${var.token}"
+  region       = "${var.region}"
+}
+
+resource "scaleway_ssh_key" "ssh_key" {
+  key = "${var.ssh_key}"
+}
+
+resource "scaleway_ip" "server_ip" {
+  server = "${scaleway_server.server.id}"
+}
+
+resource "scaleway_ip" "client_ips" {
+  count   = "${var.count}"
+  server  = "${element(scaleway_server.clients.*.id, count.index)}"
+}
+
+resource "scaleway_server" "server" {
+  name        = "load-server"
+  image       = "31dfef82-9b45-4b01-9656-031617f36599" // Debian Stretch
+  type        = "C1"
+  depends_on  = ["scaleway_ssh_key.ssh_key"]
+}
+
+resource "scaleway_server" "clients" {
+  count       = "${var.count}"
+  name        = "load-client-${count.index}"
+  image       = "31dfef82-9b45-4b01-9656-031617f36599"
+  type        = "C1"
+  depends_on  = ["scaleway_server.server"]
+}
+
+data "template_file" "ansible_inventory_tpl" {
+  template = "${file("inventory.tpl")}"
+
+  vars {
+    server_ip   = "${scaleway_ip.server_ip.ip}"
+    client_ips  = "${join("\n", scaleway_ip.client_ips.*.ip)}"
+  }
+}
+
+resource "local_file" "ansible_inventory" {
+  content   = "${data.template_file.ansible_inventory_tpl.rendered}"
+  filename  = "inventory"
+}
